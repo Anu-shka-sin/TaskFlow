1a96d3d03faa533687bf13b1a4bc154314db48a3
Anushka singh <161745755+Anu-shka-sin@users.noreply.github.com>
2026-06-29 16:06:52 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -19,8 +19,7 @@ const Layout = ({ onLogout, user }) => {
 
       if (!token) throw new Error("No auth token found");
 
-      const { data } = await axios.get(
-        "http://localhost:4000/api/tasks/gp",
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/api/tasks`),
         {
           headers: {
             Authorization: `Bearer ${token}`,
